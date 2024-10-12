ecf3f37f8efc780617fdd1221a9f91f71365218b
SGVp15 <phosphorusp15@mail.ru>
2024-10-12 08:56:23 +0300

    main

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main(path):
     click_property()
 
 
-def wait_windows(name_like: str, time_second=10):
+def wait_windows(name_like: str, time_second=5):
     is_win_activate = False
     max_sec = time_second * 10
     n = 0
@@ -85,8 +85,8 @@ if __name__ == '__main__':
                    os.path.isdir(os.path.join(os.path.join(BASE_PATH, exam), f))]
         for num in folders:
             full_path = os.path.join(BASE_PATH, exam, num)
-            subprocess.run(["C:/Program Files/iSpring/Suite 11/bin32/ispringquizmaker.exe",
-                            os.path.join(BASE_PATH, exam, f'{exam}_{num}.quiz')])
+            # subprocess.run(["C:/Program Files/iSpring/Suite 11/bin32/ispringquizmaker.exe",
+            #                 os.path.join(BASE_PATH, exam, f'{exam}_{num}.quiz')])
             wait_windows(f'{exam}_{num} - iSpring QuizMaker', time_second=999999999)
             main(full_path)
 
